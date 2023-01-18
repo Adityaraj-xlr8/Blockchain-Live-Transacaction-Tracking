a8fa9d863990a6653765bfc61f4d4c4c7d106cf2
Adityaraj-xlr8 <adityaraj2001025@gmail.com>
2023-01-18 14:15:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const notifier=require('node-notifier')
+const app=express()
+const port=3000
+
+app.use(express.json())
+
+app.post('/webhook',(req,res)=>{
+    const webhook=req.body;
+
+    for(const erc20Transfer of webhook.erc20Transfers){
+        const addr=`${erc20Transfer.to.slice(0,4)}...${erc20Transfer.to.slice(38)}`;
+        const amount=Number(erc20Transfer.valueWithDecimals).toFixed(0)
+
+        notifier.notify({
+            title: 'New USDT transfer',
+            message: `${addr} just sent \n$${amount}`
+        });
+    }
+    
+    return res.status(200).json();
+})
+
+app.listen(port,()=>{
+    console.log(`listning at ${port}`)
+})
